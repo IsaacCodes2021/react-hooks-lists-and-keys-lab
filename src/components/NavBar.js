8fec132de55957450d18119df33a05fda01657ee
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-20 15:52:34 -0700

    commit tree 01af73e232551b897543820bc481cccea9fbca18

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  let linx = links.map((link, index) => {
+    return <a href={'#' +link} key={index}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+    {linx}
+  </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,9 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((proj, index) => <ProjectItem name={proj.name} about={proj.about} technologies={proj.technologies} key={index}/>)}
+      </div>
     </div>
   );
 }
